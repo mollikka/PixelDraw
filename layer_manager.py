3dd317d1e20adbebf21cc0aa2d354afb8708cb7d
Lauri Tervonen <lauri.tervonen@aalto.fi>
2014-05-08 15:20:48 +0300

    commented layer manager code

--- layer_manager.py
+++ layer_manager.py
@@ -3,30 +3,57 @@ import pygame
 from ui.button import Button
 
 class LayerManager(object):
+    '''
+        A LayerManager sets up a system of layers that can store pictures and
+        be drawn on. Basically, it's a representation of the piece of art
+        that's being worked on.
+
+        Most tools and other picture manipulation effects should use
+        LayerManager().get_layer().get_surface() to get the pygame surface to
+        alter
+    '''
 
     def __init__(self):
 
         self.layers = [Layer(self,800,600) for i in range(10)]
         self.curlayer = 0
-        self.layer_dialog = LayerDialog(self)
-        self.picture_position = [400,100]
 
+        #background surface is shown if all layers are transparent
         self.background = pygame.Surface((800,600))
         self.background.fill((20,20,20))
 
+        #a graphical UI for choosing a layer
+        self.layer_dialog = LayerDialog(self)
+
+        #camera panning position
+        self.picture_position = [400,100]
+
     def get_layer(self):
+        '''
+            Returns the layer object that the user has chosen for editing
+        '''
 
         return self.layers[self.curlayer]
 
     def set_layer(self, layer_index):
+        '''
+            Changes the active editing layer
+        '''
 
         self.curlayer = layer_index
 
     def pick_layer(self):
+        '''
+            Input handling event for clicking the layer dialog,
+            called by handle_input
+        '''
 
         self.layer_dialog.pick_layer()
 
     def draw_picture(self, window):
+        '''
+            Renders the whole picture to window
+        '''
 
         window.blit(self.background, self.picture_position)
 
@@ -34,11 +61,18 @@ class LayerManager(object):
             layer.draw(window)
 
     def pan(self, mouse_delta):
+        '''
+            Input handling event for panning the picture,
+            called by handle_input
+        '''
 
         self.picture_position[0] += mouse_delta[0]
         self.picture_position[1] += mouse_delta[1]
 
     def draw(self, window):
+        '''
+            Renders the layer dialog elements to window
+        '''
 
         myfont = pygame.font.SysFont("monospace", 15)
 
@@ -64,9 +98,7 @@ class Layer(object):
     def draw(self, window):
 
         pictureloc = self.layer_manager.picture_position
-
         loc = [self.location[i] + pictureloc[i] for i in range(2)]
-
         window.blit(self.surface, loc)
 
     def get_mouse_pos(self):
