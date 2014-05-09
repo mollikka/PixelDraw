6a4264f4bc3dc5850e7f1ee9143b3241f8e3ff6b
Lauri Tervonen <lauri.tervonen@aalto.fi>
2014-05-09 13:47:37 +0300

    commented system code

--- system.py
+++ system.py
@@ -9,15 +9,20 @@ class System(object):
 
     def __init__(self):
 
+        #initialize the window
         self.winflags = pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE
         self.background = (0,0,0)
         self.resize_window((640,480))
 
+        #initialize the manager objects for each module
         self.tool_manager = ToolManager()
         self.color_manager = ColorManager()
         self.layer_manager = LayerManager()
 
     def resize_window(self, size):
+        '''
+            Initialize or resize the application window. Size as tuple (w,h)
+        '''
 
         self.window=pygame.display.set_mode(size,self.winflags)
         self.window.fill(self.background)
@@ -31,18 +36,20 @@ class System(object):
             color = self.color_manager.get_color()
             layer = self.layer_manager.get_layer()
 
+            #handle events
             for event in pygame.event.get():
                 handle(event,self,self.tool_manager,self.color_manager,self.layer_manager)
 
             self.tool_manager.step(layer, color)
 
+            #clear the screen
             self.window.fill(self.background)
             self.layer_manager.draw_picture(self.window)
 
+            #ask each manager object to draw the stuff related to their module
             self.layer_manager.draw(self.window)
             self.color_manager.draw(self.window)
             self.tool_manager.draw(self.window)
 
-
-
+            #update the screen
             pygame.display.flip()
