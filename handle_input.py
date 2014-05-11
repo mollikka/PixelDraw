fa170e170a34b9ac18c7d224ba5c84a0a5941b0d
Lauri Tervonen <lauri.tervonen@aalto.fi>
2014-05-11 17:52:51 +0300

    handle input code easier to read

--- handle_input.py
+++ handle_input.py
@@ -11,29 +11,40 @@ def handle(event, system, tool_manager, color_manager, layer_manager):
     #(because it's relative to the last call)
     mouse_delta = pygame.mouse.get_rel()
 
+    #MOUSE HOLD
+
     mouse_pressed = pygame.mouse.get_pressed()
     mouse_left_pressed, mouse_mid_pressed, mouse_right_pressed = mouse_pressed
 
     if mouse_right_pressed:
         layer_manager.pan(mouse_delta)
 
-
-    if event.type == pygame.QUIT:
-        sys.exit(0)
+    #MOUSE PRESS
 
     elif event.type == pygame.MOUSEBUTTONDOWN:
+        #left button
         if event.button == 1:
             tool_manager.start_drawing()
             color_manager.pick_color()
             layer_manager.pick_layer()
 
     elif event.type == pygame.MOUSEBUTTONUP:
-        tool_manager.stop_drawing()
-        tool_manager.pick_tool()
+        #left button
+        if event.button == 1:
+            tool_manager.stop_drawing()
+            tool_manager.pick_tool()
 
-    elif event.type == pygame.VIDEORESIZE:
-        system.resize_window(event.dict['size'])
+    #KEY PRESS
 
     elif event.type == pygame.KEYDOWN:
         if event.key == pygame.K_ESCAPE:
             sys.exit(0)
+
+    #SYSTEM EVENTS
+
+    elif event.type == pygame.QUIT:
+        sys.exit(0)
+
+    elif event.type == pygame.VIDEORESIZE:
+        system.resize_window(event.dict['size'])
+
