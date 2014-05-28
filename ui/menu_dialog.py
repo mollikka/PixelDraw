1a710ec22dfed54192e6b50ec87d7198d2143872
Lainism <minttu.makinen@aalto.fi>
2014-05-28 20:15:49 +0300

    new file adding

--- ui/menu_dialog.py
+++ ui/menu_dialog.py
@@ -1,11 +1,12 @@
 import tkFileDialog
+import pygame
 
-from pygame import Surface, image
-from Tkinter import Tk
+from Tkinter import Tk, Entry, Label
 from Tkinter import Button as Tkbutton
 from tools.tool import Tool
 from ui.button import Button as Ownbutton
 from ui.ui_element import UIElement
+
 
 class MenuDialog(UIElement):
 
@@ -14,6 +15,7 @@ class MenuDialog(UIElement):
         buttons = [MenuButton(ui_manager, (0,0))]
 
         super(MenuDialog, self).__init__(bounding_box, buttons)
+
 
 class MenuPopup(object):
 
@@ -39,7 +41,41 @@ class MenuPopup(object):
         self.menuwindow.mainloop()
 
     def new_file(self):
-        pass
+
+        self.res_dialog = Tk()
+
+        self.wbox = Entry(self.res_dialog)
+        self.hbox = Entry(self.res_dialog)
+
+        wlabel = Label(self.res_dialog, text="Width: ")
+        hlabel = Label(self.res_dialog, text="Height: ")
+        
+        ok = Tkbutton(self.res_dialog, text="OK", command=self.ok)
+        cancel = Tkbutton(self.res_dialog, text="Cancel", command=self.cancel)
+
+        wlabel.pack()
+        self.wbox.pack()
+        hlabel.pack()
+        self.hbox.pack()
+        ok.pack()
+        cancel.pack()
+
+        self.res_dialog.mainloop()
+
+    def ok(self):
+
+        w = int(self.wbox.get())
+        h = int(self.hbox.get())
+
+        surface = pygame.Surface((w,h), pygame.SRCALPHA)
+        self.layer_manager.new_image(surface)
+
+        self.res_dialog.destroy()
+        self.menuwindow.destroy()
+
+    def cancel(self):
+
+        self.res_dialog.destroy()
 
     def load(self):
 
@@ -48,11 +84,10 @@ class MenuPopup(object):
    
         #load image
         if filename:
-            surf = image.load(filename)
+            surf = pygame.image.load(filename)
             self.layer_manager.new_image(surf)
             self.history_manager.clear_history()
             self.menuwindow.destroy()
-
 
     def save_as(self):
 
@@ -67,7 +102,7 @@ class MenuPopup(object):
             open(filename, 'w')
             surface = self.layer_manager.background.copy()
             self.layer_manager.draw_picture(surface)
-            image.save(surface, filename)
+            pygame.image.save(surface, filename)
             self.menuwindow.destroy()
 
 
@@ -75,7 +110,7 @@ class MenuButton(Ownbutton):
 
     def __init__(self, ui_manager, topleft):
 
-        super(MenuButton, self).__init__(image.load('images/menu.png'),topleft)
+        super(MenuButton, self).__init__(pygame.image.load('images/menu.png'),topleft)
         self.ui_manager = ui_manager
 
     def activate(self, mouse_position):
